72fdb8bc54a96f4a0d5300e9a556b0ad7e073d92
Team 8 <team8@Teams-iMac.local>
2019-01-14 20:39:42 -0800

    fixed oopsiedoopsie?

--- src/main/java/com/palyrobotics/frc2019/auto/modes/Good/RightStartFrontCargoDepotFirstCargo.java
+++ src/main/java/com/palyrobotics/frc2019/auto/modes/Good/RightStartFrontCargoDepotFirstCargo.java
@@ -1,7 +1,6 @@
 package com.palyrobotics.frc2019.auto.modes.Good;
 
 import com.palyrobotics.frc2019.auto.AutoModeBase;
-import com.palyrobotics.frc2019.auto.modes.RightStartRightFrontCargoShip;
 import com.palyrobotics.frc2019.behavior.ParallelRoutine;
 import com.palyrobotics.frc2019.behavior.Routine;
 import com.palyrobotics.frc2019.behavior.SequentialRoutine;
