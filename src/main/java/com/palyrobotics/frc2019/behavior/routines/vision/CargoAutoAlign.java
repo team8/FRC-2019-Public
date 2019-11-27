05d616a0d65ef70ae83b2f2c7bdd4f46da46366f
Aditya-Paly <aoberai05@gmail.com>
2019-11-26 23:39:21 -0800

    Added data recieving end for roborio. It may or may not work and would have to be tested tomorrow. Temporarily, get data is put inside the routine itself but later will probably be moved to robot state

--- src/main/java/com/palyrobotics/frc2019/behavior/routines/vision/CargoAutoAlign.java
+++ src/main/java/com/palyrobotics/frc2019/behavior/routines/vision/CargoAutoAlign.java
@@ -9,62 +9,79 @@ import com.palyrobotics.frc2019.subsystems.Subsystem;
 import com.palyrobotics.frc2019.util.SparkDriveSignal;
 import com.palyrobotics.frc2019.util.config.Configs;
 import com.palyrobotics.frc2019.util.csvlogger.CSVWriter;
+import org.apache.commons.lang3.SerializationUtils;
 
+import java.util.HashMap;
+
+/**
+ * Auto aligns to closest cargo using p loop and data from odroid microcontroller
+ *
+ * @author Aditya Oberai
+ */
 public class CargoAutoAlign extends Routine {
 
-	private SparkDriveSignal motor = new SparkDriveSignal(); // TODO Change to Lazy Spark Max and add forward movement
-																// once aligned until cargo has been picked up
+    private SparkDriveSignal motor = new SparkDriveSignal();
 
-	@Override
-	public void start() {
+    @Override
+    public void start() {
 
-	}
+    }
 
-	@Override
-	public Commands update(Commands commands) {
-		double pLoop = Configs.get(VisionConfig.class).autoAlignGains.f
-				* (getError() * Configs.get(VisionConfig.class).autoAlignGains.p);
-		if (Math.abs(pLoop) < 50) {
-			System.out.println("Completely aligned");
-			commands.wantedIntakeState = Intake.IntakeMacroState.DOWN_FOR_GROUND_INTAKE;
-		} else if (pLoop < 0) {
-			System.out.println("Turning left");
-			// motor.leftOutput.setPercentOutput(pLoop);
-			// motor.rightOutput.setPercentOutput(-pLoop);
-		} else if (pLoop > 0) {
-			System.out.println("Turning right");
-			// motor.leftOutput.setPercentOutput(pLoop);
-			// motor.rightOutput.setPercentOutput(-pLoop);
-		}
-		CSVWriter.addData("AutoAlignMotorOutput", pLoop);
-		return commands;
-	}
+    @Override
+    public Commands update(Commands commands) {
+        int error = (int) getData().get("Error");
+        double pLoop = Configs.get(VisionConfig.class).autoAlignGains.f
+                * (error * Configs.get(VisionConfig.class).autoAlignGains.p);
+        if (Math.abs(pLoop) < 50) {
+            System.out.println("Completely aligned");
+            commands.wantedIntakeState = Intake.IntakeMacroState.DOWN_FOR_GROUND_INTAKE;
+        } else if (pLoop < 0) {
+            System.out.println("Turning left");
+            // motor.leftOutput.setPercentOutput(pLoop);
+            // motor.rightOutput.setPercentOutput(-pLoop);
+        } else if (pLoop > 0) {
+            System.out.println("Turning right");
+            // motor.leftOutput.setPercentOutput(pLoop);
+            // motor.rightOutput.setPercentOutput(-pLoop);
+        }
+        CSVWriter.addData("AutoAlignMotorOutput", pLoop);
+        return commands;
+    }
 
-	@Override
-	public Commands cancel(Commands commands) {
-		return commands;
-	}
+    @Override
+    public Commands cancel(Commands commands) {
+        return commands;
+    }
 
-	@Override
-	public boolean isFinished() {
-		return false;
-	}
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 
-	@Override
-	public Subsystem[] getRequiredSubsystems() {
-		return new Subsystem[0];
-	}
+    @Override
+    public Subsystem[] getRequiredSubsystems() {
+        return new Subsystem[0];
+    }
 
-	@Override
-	public String getName() {
-		return "CargoAutoAlign";
-	}
+    @Override
+    public String getName() {
+        return "CargoAutoAlign";
+    }
 
-	/**
-	 * Gets error from odroid sent UDP packet.
-	 */
-	public static int getError() {
-		String errorStr = RobotState.getInstance().commandReceiver.executeCommand("get");
-		return Integer.parseInt(errorStr);
-	}
+    /**
+     * Gets error from odroid sent UDP packet.
+     */
+    public static HashMap getData() {
+        String errorStr = RobotState.getInstance().commandReceiver.executeCommand("get");
+        System.out.println(errorStr);
+        byte[] input = errorStr.getBytes();
+									/*
+									TODO
+									This may or may not work
+									My issue is that the command receiver returns a string when I send bytes so some sort of conversion has to be made first.
+									*/
+        HashMap visionData = (HashMap) SerializationUtils.deserialize(input);
+        System.out.println("Cargo located at" + visionData.get("CentroidPoint"));
+        return visionData;
+    }
 }
