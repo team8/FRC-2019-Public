43e6d8fe7b2cd01244987adfc9fab7d8a5d53c37
Vedanth P <vedpadu@gmail.com>
2020-01-01 21:52:42 -0800

    finished deserealization of autos except for parallel routines

--- src/main/java/com/palyrobotics/frc2019/auto/DeserializeAutos.java
+++ src/main/java/com/palyrobotics/frc2019/auto/DeserializeAutos.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Annotation;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -26,12 +27,17 @@ public class DeserializeAutos {
 
 	public void deserialize(String autoClassName, JsonNode jsonNode) throws JSONException, IOException {
 		JSONObject autoObject = new JSONObject(jsonNode.toString());
+		JSONArray autoArray = autoObject.getJSONArray("routines");
 		System.out.println(getParamNames("DrivePathRoutine", "drive") + autoObject.toString());
 
 		// setup of class
 		String key = "";
 		String nameString = "";
 		String imports = "package com.palyrobotics.frc2019.auto.modes;\n";
+		imports += "import com.palyrobotics.frc2019.auto.AutoModeBase;\n"
+				+ "import com.palyrobotics.frc2019.behavior.ParallelRoutine;\n"
+				+ "import com.palyrobotics.frc2019.behavior.Routine;\n"
+				+ "import com.palyrobotics.frc2019.behavior.SequentialRoutine;";
 		String filePath = "/src/main/java/com/palyrobotics/frc2019/auto/modes/" + autoClassName + ".java";
 		File autoFile = new File(System.getProperty("user.dir") + filePath);
 		BufferedReader autoReader = new BufferedReader(new FileReader(autoFile));
@@ -65,12 +71,155 @@ public class DeserializeAutos {
 		System.out.println(key + " key");
 		//
 		// routine and imports
-		String routineSetup;
-		String routineString;
+		String routineSetup = "";
+		String routineString = "return new SequentialRoutine(";
+		for (var i = 0; i < autoArray.length(); i++) {
+			JSONObject routineObject = autoArray.getJSONObject(i);
+			String type = (String) routineObject.get("@type");
+			if (type.equals("ParallelRoutine")) {
+
+			} else {
+				String[] typeSplit = type.split("\\.");
+				String[] paramNames;
+				String[] paramTypes;
+				String newRoutineString = "new ";
+				if (typeSplit.length > 1) {
+					paramNames = getParamNames(typeSplit[1], typeSplit[0]).split(",");
+					paramTypes = getParamTypes(typeSplit[1], typeSplit[0]).split(",");
+					imports += "import com.palyrobotics.frc2019.behavior.routines." + typeSplit[0] + "." + typeSplit[1]
+							+ ";\n";
+					newRoutineString += typeSplit[1] + "(";
+				} else {
+					paramNames = getParamNames(typeSplit[0], "null").split(",");
+					paramTypes = getParamTypes(typeSplit[0], "null").split(",");
+					newRoutineString += typeSplit[0] + "(";
+					imports += "import com.palyrobotics.frc2019.behavior.routines." + typeSplit[0] + ";\n";
+				}
+
+				for (var j = 0; j < paramTypes.length; j++) {
+					switch (paramTypes[j]) {
+						case "double":
+						case "boolean":
+						case "int": {
+							newRoutineString += routineObject.get(paramNames[j]);
+							if (j != paramTypes.length - 1) {
+								newRoutineString += ",";
+							}
+							continue;
+						}
+						case "String": {
+							newRoutineString += "\"" + routineObject.get(paramNames[j]) + "\"";
+							if (j != paramTypes.length - 1) {
+								newRoutineString += ",";
+							}
+							continue;
+						}
+						case "Path": {
+							JSONArray pathArray = routineObject.getJSONArray(paramNames[j]);
+							String makePathVar = "List<Path.Waypoint> path" + i + j + " = new ArrayList<>();\n";
+							imports += "import java.util.ArrayList;\n" + "import java.util.List;\n";
+							imports += "import com.palyrobotics.frc2019.util.trajectory.*;\n";
+							routineSetup += makePathVar;
+							for (var k = 0; k < pathArray.length(); k++) {
+								JSONArray waypointArray = pathArray.getJSONObject(k).getJSONArray("Waypoint");
+								String addWayPoint = "path" + i + j + ".add(new Path.Waypoint(new Translation2d("
+										+ waypointArray.getJSONObject(0).get("x") + ","
+										+ waypointArray.getJSONObject(1).get("y") + "),"
+										+ waypointArray.getJSONObject(2).get("speed") + "));\n";
+								routineSetup += addWayPoint;
+							}
+							newRoutineString += "new Path(path" + i + j + ")\n";
+							if (j != paramTypes.length - 1) {
+								newRoutineString += ",";
+							}
+							continue;
+						}
+						case "SparkDriveSignal": {
+							JSONArray sparkDriveArray = routineObject.getJSONArray(paramNames[j]);
+							double leftOut = sparkDriveArray.getJSONObject(0).getDouble("leftOutput");
+							double rightOut = sparkDriveArray.getJSONObject(1).getDouble("rightOutput");
+							routineSetup += "SparkMaxOutput left" + i + j + " = new SparkMaxOutput();\n";
+							routineSetup += "left" + i + j + ".setPercentOutput(" + leftOut + ");\n";
+							routineSetup += "SparkMaxOutput right" + i + j + " = new SparkMaxOutput();\n";
+							routineSetup += "right" + i + j + ".setPercentOutput(" + rightOut + ");\n";
+							newRoutineString += "new SparkDriveSignal(left" + i + j + ", right" + i + j + ")\n";
+							imports += "import com.palyrobotics.frc2019.util.SparkDriveSignal;\n"
+									+ "import com.palyrobotics.frc2019.util.SparkMaxOutput;\n";
+						}
+
+					}
+				}
+				newRoutineString += ")";
+				if (i == autoArray.length() - 1) {
+					routineString += newRoutineString + ");";
+				} else {
+					newRoutineString += ",";
+					routineString += newRoutineString;
+				}
+			}
+			System.out.println(routineString);
+			System.out.println(routineSetup);
+		}
 		//
 		// putting all together
+		try {
+			FileWriter fW = new FileWriter(new File(System.getProperty("user.dir")
+					+ "\\src\\main\\java\\com\\palyrobotics\\frc2019\\auto\\modes\\" + autoClassName + ".java"));
+
+			String toStringFunc = "";
+			String getRoutineFunc = "";
+			imports += "\n";
+			toStringFunc += "@Override\n" + "\tpublic String toString() {\n"
+					+ "\t\treturn sAlliance + this.getClass().toString();\n" + "\t}\n";
+			String preStartFunc = "";
+			preStartFunc += "\t@Override\n" + "\tpublic void preStart() {}\n" + "\n";
+
+			getRoutineFunc += "\t@Override\n" + "\tpublic Routine getRoutine() {\n" + routineSetup + routineString
+					+ "}";
+			String getKeyFunc = "";
+			getKeyFunc += "\t@Override\n" + "\tpublic String getKey() {" + key + "\t}";
+			fW.write(imports + "public class test1 extends AutoModeBase{\n" + toStringFunc + preStartFunc
+					+ getRoutineFunc + getKeyFunc + "\n}");
+			fW.close();
+		} catch (IOException e) {
+		}
 
 		//
+	}
+
+	String getParamTypes(String routineName, String routinePackage) {
+		try {
+			Class aClass;
+			if (routinePackage != "null") {
+				aClass = Class
+						.forName("com.palyrobotics.frc2019.behavior.routines." + routinePackage + "." + routineName);
+			} else {
+				aClass = Class.forName("com.palyrobotics.frc2019.behavior.routines." + routineName);
+			}
+
+			String paramTypes = "";
+			int constructorIndex = -1;
+			for (var j = 0; j < aClass.getConstructors().length; j++) {
+				if (aClass.getConstructors()[j].getDeclaredAnnotations().length > 0) {
+					Annotation annot = aClass.getConstructors()[j].getAnnotation(JsonCreator.class);
+					if (annot instanceof JsonCreator) {
+						constructorIndex = j;
+					}
+				}
+			}
+			if (constructorIndex != -1) {
+				for (var i = 0; i < aClass.getConstructors()[constructorIndex].getParameterTypes().length; i++) {
+					paramTypes += aClass.getConstructors()[constructorIndex].getParameterTypes()[i].getSimpleName()
+							+ ",";
+				}
+				return paramTypes;
+			} else {
+				return "constructor not found";
+			}
+
+		} catch (ClassNotFoundException e) {
+			return "e";
+		}
 	}
 
 	String getParamNames(String routineName, String routinePackage) {
--- src/main/java/com/palyrobotics/frc2019/auto/modes/test1.java
+++ src/main/java/com/palyrobotics/frc2019/auto/modes/test1.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.palyrobotics.frc2019.auto.AutoModeBase;
-import com.palyrobotics.frc2019.behavior.ParallelRoutine;
 import com.palyrobotics.frc2019.behavior.Routine;
 import com.palyrobotics.frc2019.behavior.SequentialRoutine;
 import com.palyrobotics.frc2019.behavior.routines.drive.BBTurnAngleRoutine;
@@ -27,21 +26,15 @@ public class test1 extends AutoModeBase {
 
 	@Override
 	public Routine getRoutine() {
-		List<Path.Waypoint> path00 = new ArrayList<>();
-		List<Path.Waypoint> path10 = new ArrayList<>();
-		List<Path.Waypoint> path40 = new ArrayList<>();
-		path00.add(new Path.Waypoint(new Translation2d(51, 0), 0));
-		path10.add(new Path.Waypoint(new Translation2d(0, 0), 33));
-		path40.add(new Path.Waypoint(new Translation2d(0, 56), 0));
-		SparkMaxOutput right = new SparkMaxOutput();
-		right.setPercentOutput(0.6);
-		SparkMaxOutput left = new SparkMaxOutput();
-		left.setPercentOutput(0.6);
-		return new SequentialRoutine(
-				new ParallelRoutine(new DrivePathRoutine(new Path(path00), false),
-						new DrivePathRoutine(new Path(path10), false)),
-				new BBTurnAngleRoutine(36), new BBTurnAngleRoutine(369), new DrivePathRoutine(new Path(path40), false),
-				new DriveTimeRoutine(100, new SparkDriveSignal(left, right)));
+		List<Path.Waypoint> path30 = new ArrayList<>();
+		path30.add(new Path.Waypoint(new Translation2d(0.0, 56.0), 0.0));
+		SparkMaxOutput left41 = new SparkMaxOutput();
+		left41.setPercentOutput(0.6);
+		SparkMaxOutput right41 = new SparkMaxOutput();
+		right41.setPercentOutput(0.6);
+		return new SequentialRoutine(new BBTurnAngleRoutine(36.0), new BBTurnAngleRoutine(369.0),
+				new DrivePathRoutine(new Path(path30), false),
+				new DriveTimeRoutine(100000, new SparkDriveSignal(left41, right41)));
 	}
 
 	@Override
